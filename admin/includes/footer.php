2b88f6b13f0291339ae57c432ccb968650e91d38
TA <tamilarasuva444@gmail.com>
2021-04-10 13:58:22 +0530

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© <script>document.write(new Date().getFullYear())</script> All Rights Reserved |  <a href="#">TMS</a> </p>
+</div>	
---
+++ payment.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+}
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>TMS | Tourism Management System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Tourism Management System In PHP" />
+    <script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- Custom Theme files -->
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!--animate-->
+    <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <style>
+        .errorWrap {
+            padding: 10px;
+            margin: 0 0 20px 0;
+            background: #fff;
+            border-left: 4px solid #dd3d36;
+            -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+            box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+        }
+
+        .succWrap {
+            padding: 10px;
+            margin: 0 0 20px 0;
+            background: #fff;
+            border-left: 4px solid #5cb85c;
+            -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+            box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+        }
+
+        .inlineimage {
+            max-width: 470px;
+            margin-right: 8px;
+            margin-left: 10px
+        }
+
+        .images {
+            display: inline-block;
+            max-width: 98%;
+            height: auto;
+            width: 22%;
+            margin: 1%;
+            left: 20px;
+            text-align: center
+        }
+    </style>
+</head>
+
+<body>
+    <!-- top-header -->
+    <div class="top-header">
+        <?php include('includes/header.php'); ?>
+        <div class="banner-1 ">
+            <div class="container">
+                <h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">TMS-Tourism Management System</h1>
+            </div>
+        </div>
+        <!--- /banner-1 ---->
+        <!--- privacy ---->
+        <div class="container">
+            <div class="page-header text-center">
+                <h1>Payment Gateway</h1>
+            </div>
+            <!-- Credit Card Payment Form - START -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-md-offset-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <h3 class="text-center">Payment Details</h3>
+                                    <div class="inlineimage"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Mastercard-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Discover-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Paypal-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/American-Express-Curved.png"> </div>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <form role="form">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <div class="form-group"> <label>CARD NUMBER</label>
+                                                <div class="input-group"> <input type="tel" class="form-control" placeholder="Valid Card Number" /> <span class="input-group-addon"><span class="fa fa-credit-card"></span></span> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-7 col-md-7">
+                                            <div class="form-group"> <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label> <input type="tel" class="form-control" placeholder="MM / YY" /> </div>
+                                        </div>
+                                        <div class="col-xs-5 col-md-5 pull-right">
+                                            <div class="form-group"> <label>CVV CODE</label> <input type="tel" class="form-control" placeholder="CVV" /> </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <div class="form-group"> <label>CARD OWNER</label> <input type="text" class="form-control" placeholder="Card Owner Name" /> </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="panel-footer">
+                                <div class="row">
+                                    <div class="col-xs-12"> <button class="btn btn-success btn-lg btn-block">Confirm Payment</button> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--- /privacy ---->
+        <!--- footer-top ---->
+        <!--- /footer-top ---->
+        <?php include('includes/footer.php'); ?>
+        <!-- signup -->
+        <?php include('includes/signup.php'); ?>
+        <!-- //signu -->
+        <!-- signin -->
+        <?php include('includes/signin.php'); ?>
+        <!-- //signin -->
+        <!-- write us -->
+        <?php include('includes/write-us.php'); ?>
+</body>
+
+</html>
